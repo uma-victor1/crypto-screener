b6e0392d550d3c813097e1d128e5baa524053511
Victor Uma <umavictor11@gmail.com>
2023-06-03 14:49:27 +0100

    feat(continious delivery): automate code delivery

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,12 +2,28 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navigation = () => {
-  return <nav className="w-[40%] mt-16 flex justify-around align-middle border border-cyan rounded-lg">
-
-    <NavLink to="/" className="w-full text-base text-center font-nunito m-2.5 bg-gray-200 text-gray-100 hover:text-cyan active:text-gray-300 border-0 cursur-pointer rounded capitalize font-semibold">Crypto</NavLink>
-    <NavLink to="/" className="w-full text-base text-center font-nunito m-2.5 bg-gray-200 text-gray-100 hover:text-cyan active:text-gray-300 border-0 cursur-pointer rounded capitalize font-semibold">Trending</NavLink>
-    <NavLink to="/" className="w-full text-base text-center font-nunito m-2.5 bg-gray-200 text-gray-100 hover:text-cyan active:text-gray-300 border-0 cursur-pointer rounded capitalize font-semibold">Saved</NavLink>
-  </nav>
+  return (
+    <nav className="w-[40%] mt-16 flex justify-around align-middle border border-cyan rounded-lg">
+      <NavLink
+        to="/"
+        className="w-full text-base text-center font-nunito m-2.5 bg-gray-200 text-gray-100 hover:text-cyan active:text-gray-300 border-0 cursur-pointer rounded capitalize font-semibold"
+      >
+        Crypto
+      </NavLink>
+      <NavLink
+        to="/trending"
+        className="w-full text-base text-center font-nunito m-2.5 bg-gray-200 text-gray-100 hover:text-cyan active:text-gray-300 border-0 cursur-pointer rounded capitalize font-semibold"
+      >
+        Latest
+      </NavLink>
+      <NavLink
+        to="/saved"
+        className="w-full text-base text-center font-nunito m-2.5 bg-gray-200 text-gray-100 hover:text-cyan active:text-gray-300 border-0 cursur-pointer rounded capitalize font-semibold"
+      >
+        Saved
+      </NavLink>
+    </nav>
+  )
 }
 
 export default Navigation
